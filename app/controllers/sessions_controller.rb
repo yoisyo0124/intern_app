135e3e1a2747740860743f65b579ac64be4da0a2
yoisyo0124 <79128366+yoisyo0124@users.noreply.github.com>
2021-02-18 17:59:30 +0900

    Add files via upload

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to new_post_path
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    log_out if logged_in?
+    redirect_to posts_path
+  end
+end
