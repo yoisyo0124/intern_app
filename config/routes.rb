5094f71ba64b9652d0a7351234fd590a57d80b9e
yoisyo0124 <79128366+yoisyo0124@users.noreply.github.com>
2021-02-19 13:06:31 +0900

    Delete config directory

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  get 'sessions/new'
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
